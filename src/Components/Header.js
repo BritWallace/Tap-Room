07fc7c5d80912ea8c69744df6d19c1aed9fec3d0
Brit Wallace <britwallace888@gmail.com>
2022-06-03 21:30:14 -0700

    add header file

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+  return (
+    <h1>Tap Site</h1>
+  );
+}
+
+export default Header;
---
+++ src/Components/TapControl.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import NewTapForm from './NewTapForm';
+import TapList from './TapList';
+import TapDetail from "./TapDetail";
+import EditTapForm from './EditTapForm';
