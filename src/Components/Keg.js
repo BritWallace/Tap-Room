f45428f6aaf1d585da8e7db3ba5a8d7493f1f857
Brit Wallace <britwallace888@gmail.com>
2022-06-05 22:02:30 -0700

    add kegList logic

--- src/Components/Keg.js
+++ src/Components/Keg.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Keg(prop){
+function Keg(props){
   return (
     <React.Fragment>
-      <div onClick = {() => whenTapClicked(props.id)}>
+      <div onClick = {() => whenKegClicked(props.id)}>
         <h3>Beer Name:{props.name}, Brand: {props.brand}</h3>
         <p>Price: ${props.price}</p>
         <p>Alcohol Content: <em>{props.alcoholContent}%</em></p>
@@ -14,13 +14,13 @@ function Keg(prop){
   );
 }
 
-Keg.propType = {
+Keg.propTypes = {
   name: PropTypes.string.isRequired,
   price: PropTypes.number,
   alcoholContent: PropTypes.number,
-  pints: PropType.number,
+  pints: PropTypes.number,
   id: PropTypes.string,
-  // whenTapClicked: PropTypes.func,
+  whenKegClicked: PropTypes.func,
 
 
   
--- src/Components/KegControl.js
+++ src/Components/KegControl.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import NewKegForm from './NewKegForm';
-// import KegList from './KegList';
+import KegList from './KegList';
 // import KegDetail from "./KegDetail";
 // import EditKegForm from './EditKegForm';
 
